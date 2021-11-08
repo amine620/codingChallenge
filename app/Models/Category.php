2a49d29e6107496a52f9ece1da66c584b7144353
amine620 <mohafidi2002@gmail.com>
2021-11-08 17:09:42 +0100

    add models with relationship and migration files

---
+++ app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
